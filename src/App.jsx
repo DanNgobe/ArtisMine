cb773b0c0ff00e6f6d027e7e0b42de3b9b04146f
DanNgobe <2556833@students.wits.ac.za>
2024-12-31 10:44:44 +0200

    chore: initial commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+const App = () => {
+  const name = 'ArtisMine';
+
+  return (
+    <div>
+      <h1>Welcome to {name}</h1>
+      <p>ArtisMine is a platform for artists to share their work and connect with other artists.</p>
+    </div>
+  );
+};
+
+export default App;
